ff1f1082ea446ae2604346e220afc070ed3bc4bc
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2021-11-21 23:44:54 -0600

    Add basic server config and mongoose connection

---
+++ server/config/config.ts
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const SERVER_PORT = parseInt(process.env.PORT!) || 5000;
+
+const DB_URI = process.env.DB_URI || 'localhost:27017';
+
+const TOKEN_SECRET = process.env.TOKEN_SECRET || 'mysecret';
+const TOKEN_ISSUER = process.env.TOKEN_ISSUER || 'uptime-monitor';
+const TOKEN_EXPIRE_TIME = process.env.TOKEN_EXPIRE_TIME || '1h';
+
+const COOKIE_MAX_AGE = parseInt(process.env.COOKIE_EXPIRE_TIME!) || 3600000;
+
+const config = {
+  server: {
+    port: SERVER_PORT,
+  },
+  db: {
+    uri: DB_URI,
+  },
+  token: {
+    secret: TOKEN_SECRET,
+    issuer: TOKEN_ISSUER,
+    expire_time: TOKEN_EXPIRE_TIME,
+  },
+  cookie: {
+    maxage: COOKIE_MAX_AGE,
+  },
+};
+
+export default config;
---
+++ server/index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+
+import config from './config/config';
+
+const app = express();
+
+mongoose.connect(config.db.uri);
+
+const db = mongoose.connection;
+db.on('error', () => {
+  console.log('Error while connecting to database');
+});
+db.once('open', () => {
+  console.log('Connection successful');
+});
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+
+app.get('/', (_: Request, res: Response) => {
+  res.send('Server is running');
+});
+
+app.listen(config.server.port, () => {
+  console.log('Server is listening on port: ' + config.server.port);
+});
